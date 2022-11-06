2b28c1aa83df7f2a33c5ff42038d6ca27ea46000
Hiro Protagonist <shd327@gmail.com>
2022-11-06 16:39:40 -0700

    added fetching code to grab date wind and humity

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var input = "bridgewater"
+var fiveDay = document.getElementById("five_day")
+
+var citySearch = document.getElementById("citySearch")
+
+var searchBtn = document.getElementById("searchBtn")
+
 
 function getAPI(input) {
     const myKey = "8d16f28b545852d623de7ad3baf04f51";
@@ -9,6 +15,22 @@ function getAPI(input) {
         })
         .then(function (data) {
             console.log(data)
+            return data
+        })
+        .then(function (data) {
+            fiveDay.textContent = "";
+            for (let i = 0; i < data.list.length; i++) {
+                var date = data.list[i].dt_txt
+                var humidity = data.list[i].main.humidity
+                var windSpeed = data.list[i].wind.speed
+                console.log(date + " " + humidity + " " + windSpeed)
+            }
         })
 }
 getAPI(input)
+
+searchBtn.addEventListener("click", function () {
+    console.log(citySearch.value)
+})
+
+// temp wind humidity
