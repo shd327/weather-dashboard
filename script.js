c1d9820ce11f1962234ed3b57550cd622034da77
Hiro Protagonist <shd327@gmail.com>
2022-11-12 13:35:48 -0700

    added localstorage code and button addeventlistner as well as modified styling

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ var currentName = ""
 var searchBtn = document.getElementById("searchBtn")
 var displayArea = document.getElementById("displayArea")
 let savedCities = localStorage.getItem('savedCities') ? JSON.parse(localStorage.getItem('savedCities')) : []
+let localStorageItems = document.getElementById('localStorageItems')
+for (let i = 0; i < savedCities.length; i++) {
+    createCityButtonsStorage(savedCities[i])
 
+}
 function getAPI(input) {
     const myKey = "8d16f28b545852d623de7ad3baf04f51";
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&cnt=5" + "&units=imperial" + "&appid=" + myKey)
@@ -14,26 +18,18 @@ function getAPI(input) {
         .then(function (response) {
             return response.json()
         })
-        .then(function (data) {
-            // console.log(data)
-            return data
-        })
+
         .then(function (data) {
 
 
             fiveDay.textContent = "";
             for (let i = 0; i < data.list.length; i++) {
-                // console.log(data.list);
-                // var date = data.list[i].dt_txt
-                // var humidity = data.list[i].main.humidity
-                // var windSpeed = data.list[i].wind.speed
-                // console.log(date + " " + humidity + " " + windSpeed)
-                // http://openweathermap.org/img/wn/${icon}.png`
+
                 var test = data.list[i].weather[0].icon;
 
                 var div = document.createElement('div');
                 div.innerHTML =
-                    `<div class="card m-2" style = "width: 12rem;">
+                    `<div class="card m-2 bg-primary" style = "width: 12rem;">
                     <img class="card-img-top" style="width: 50px; height: 50px;" src="http://openweathermap.org/img/wn/${test}.png" alt="${data.list[i].weather[0].description}">
                     <div class="card-body">
                     <h5 class="card-title">Temperature: ${data.list[i].main.temp}</h5>
@@ -50,6 +46,12 @@ function getAPI(input) {
 
 searchBtn.addEventListener("click", function () {
     var citySearch = document.getElementById("citySearch").value
+    if (citySearch === "") {
+        NotACity()
+        return
+
+    }
+
     getAPI(citySearch)
     fetchingCurrentLocation(citySearch)
 })
@@ -66,7 +68,13 @@ function fetchingCurrentLocation(citySearch) {
         })
         .then(function (data) {
 
+            if (data.cod === "404") {
+                NotACity()
 
+            }
+            else {
+                validateCity(data.name)
+            }
             return data;
 
         }).then(function (data) {
@@ -80,18 +88,25 @@ function fetchingCurrentLocation(citySearch) {
                 })
 
                 .then(function (data) {
+                    displayArea.textContent = ""
                     console.log(data)
                     var humidity = document.createElement('h3')
                     var wind = document.createElement('h3')
                     var cityName = document.createElement('h1')
                     var temperature = document.createElement('h3')
                     var uvData = document.createElement('h3')
+                    var img = document.createElement("img")
                     humidity.textContent = data.current.humidity
                     var dateConversion = new Date(data.current.dt * 1000).toLocaleString();
                     wind.textContent = data.current.wind_speed
                     cityName.textContent = currentName + " " + dateConversion
                     temperature.textContent = data.current.temp
                     uvData.textContent = data.current.uvi
+                    uvData.style.width = "4%"
+                    var icon = data.current.weather[0].icon
+
+                    let displayIcon = `http://openweathermap.org/img/wn/${icon}.png`
+                    img.src = displayIcon;
 
                     if (data.current.uvi < 2) {
                         uvData.classList.add("greenBox")
@@ -112,6 +127,7 @@ function fetchingCurrentLocation(citySearch) {
 
 
                     displayArea.appendChild(cityName)
+                    displayArea.appendChild(img)
                     displayArea.appendChild(temperature)
                     displayArea.appendChild(wind)
                     displayArea.appendChild(humidity)
@@ -128,3 +144,66 @@ function fetchingCurrentLocation(citySearch) {
 
 
 
+function validateCity(data) {
+    let city = document.querySelector('#citySearch').value
+
+
+    if (localStorage.getItem('savedCities')) {
+
+        savedCities = JSON.parse(localStorage.getItem('savedCities'))
+    }
+
+    let flag = false;
+    for (let i = 0; i < savedCities.length; i++) {
+        if (savedCities[i] === city) {
+
+            flag = true;
+            break;
+
+        }
+
+    }
+    if (flag === false) {
+        savedCities.push(city)
+        localStorage.setItem('savedCities', JSON.stringify(savedCities))
+        createCityButtonsStorage(city)
+        console.log(city)
+    }
+
+    return data;
+
+}
+
+
+function NotACity() {
+    alert("This City doesn't exist")
+
+    return;
+}
+
+function createCityButtonsStorage(city) {
+    var column = document.createElement('div')
+    column.classList.add('col-12')
+    column.classList.add('button-group')
+
+    var cityButton = document.createElement('button')
+    cityButton.classList.add('btn')
+    cityButton.classList.add("btn-secondary")
+    cityButton.textContent = city
+    column.appendChild(cityButton)
+    localStorageItems.appendChild(column)
+    var buttonPress = document.getElementsByClassName("btn-secondary");
+    buttonPressed(buttonPress);
+
+
+}
+
+function buttonPressed(buttonPress) {
+    for (let i = 0; i < buttonPress.length; i++) {
+        buttonPress[i].addEventListener("click", function (event) {
+            getAPI(event.target.textContent)
+            fetchingCurrentLocation(event.target.textContent)
+        })
+    }
+
+}
